275f8896c91ddc9779a5bd782b1427772c304a74
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-02-06 15:40:07 +0200

    redesign area buttons

--- mod.js
+++ mod.js
@@ -1,4 +1,4 @@
-const CHANGED_AREA_BUTTON_POSITIONS = {
+const CHANGED_BUTTON_POSITIONS = {
   arid: { x: 454, y: 213 },
   'autumn-area': { x: 259, y: 158 },
   'autumn-fall': { x: 329, y: 163 },
@@ -9,31 +9,80 @@ const CHANGED_AREA_BUTTON_POSITIONS = {
   jungle: { x: 279, y: 113 },
 };
 
-for (let id in CHANGED_AREA_BUTTON_POSITIONS) {
-  if (Object.prototype.hasOwnProperty.call(CHANGED_AREA_BUTTON_POSITIONS, id)) {
-    let pos = CHANGED_AREA_BUTTON_POSITIONS[id];
+for (let id in CHANGED_BUTTON_POSITIONS) {
+  if (Object.prototype.hasOwnProperty.call(CHANGED_BUTTON_POSITIONS, id)) {
+    let pos = CHANGED_BUTTON_POSITIONS[id];
     sc.map.areas[id].position = pos;
   }
 }
 
+const BUTTON_SPRITE = {
+  srcX: 0,
+  srcY: 0,
+  width: 10,
+  height: 10,
+  posX: 3,
+  posY: 3,
+};
+
+const CROSSHAIR_SPRITE = {
+  srcX: 14,
+  srcY: 39,
+  width: 25,
+  height: 25,
+  posX: -5,
+  posY: -4,
+};
+
+const BUTTON_HIGHLIGHT_SPRITE = {
+  srcX: 39,
+  srcY: 39,
+  width: CROSSHAIR_SPRITE.width,
+  height: CROSSHAIR_SPRITE.height,
+  posX: CROSSHAIR_SPRITE.posX,
+  posY: CROSSHAIR_SPRITE.posY,
+};
+
 sc.AreaButton.inject({
   patchedGfx: new ig.Image('media/gui/patched-area-buttons.png'),
+
   updateDrawables(renderer) {
     if (this.focus) {
       renderer
-        .addGfx(this.patchedGfx, -3, -2, 24, 0, 21, 21)
+        .addGfx(
+          this.patchedGfx,
+          BUTTON_HIGHLIGHT_SPRITE.posX,
+          BUTTON_HIGHLIGHT_SPRITE.posY,
+          BUTTON_HIGHLIGHT_SPRITE.srcX,
+          BUTTON_HIGHLIGHT_SPRITE.srcY,
+          BUTTON_HIGHLIGHT_SPRITE.width,
+          BUTTON_HIGHLIGHT_SPRITE.height,
+        )
         .setCompositionMode('lighter');
+      renderer.addGfx(
+        this.patchedGfx,
+        CROSSHAIR_SPRITE.posX,
+        CROSSHAIR_SPRITE.posY,
+        CROSSHAIR_SPRITE.srcX,
+        CROSSHAIR_SPRITE.srcY,
+        CROSSHAIR_SPRITE.width,
+        CROSSHAIR_SPRITE.height,
+      );
     }
+
     renderer.addGfx(
       this.patchedGfx,
-      4,
-      4,
-      0 + this.icon,
-      0 + (this.activeArea ? 8 : 0),
-      8,
-      8,
+      BUTTON_SPRITE.posX,
+      BUTTON_SPRITE.posY,
+      BUTTON_SPRITE.srcX +
+        sc.AREA_TYPE[this.area.areaType] * BUTTON_SPRITE.width,
+      BUTTON_SPRITE.srcY + (this.activeArea ? 1 : 0) * BUTTON_SPRITE.height,
+      BUTTON_SPRITE.width,
+      BUTTON_SPRITE.height,
     );
+
     if (this.activeArea) {
+      // hardcoded coordinates from game.compiled.js
       renderer.addGfx(this.gfx, 1, 2, 304, 440, 3, 3);
       renderer.addGfx(this.gfx, -11, -8, 280, 424, 16, 11);
     }
